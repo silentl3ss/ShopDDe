1be6a9d7cb47d3b670547866ed0ac35e54653ff7
silentl3ss <anhduydo23@gmail.com>
2024-11-24 16:49:59 +0700

    updated
    Signed-off-by: silentl3ss <anhduydo23@gmail.com>

--- lib/views/screens/main_screen.dart
+++ lib/views/screens/main_screen.dart
@@ -11,7 +11,16 @@ class _MainScreenState extends State<MainScreen> {
   @override
   Widget build(BuildContext context){
     return Scaffold(
-      body: Center(child: Text('Welcome'),),
+      bottomNavigationBar: BottomNavigationBar(
+        type: BottomNavigationBarType.fixed,
+          items: [
+        BottomNavigationBarItem(icon: Image.asset("assets/icons/home.png", width: 25,),label: "Home"),
+        BottomNavigationBarItem(icon: Image.asset("assets/icons/love.png",width: 25,),label: "Favorite"),
+        BottomNavigationBarItem(icon: Image.asset("assets/icons/mart.png",width: 25,),label: "Stores"),
+        BottomNavigationBarItem(icon: Image.asset("assets/icons/cart.png",width: 25,),label: "Cart"),
+        BottomNavigationBarItem(icon: Image.asset("assets/icons/user_main.png",width: 25,),label: "Account"),
+      ]),
+
     );
 
   }
